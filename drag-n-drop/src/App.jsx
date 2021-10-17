5efccc35adf606d83f8ef26bb7d632eb5115946d
Sergii Bogush <sergbogush@gmail.com>
2021-10-17 17:26:17 +0300

    Update App.jsx

--- drag-n-drop/src/App.jsx
+++ drag-n-drop/src/App.jsx
@@ -8,10 +8,59 @@ const App = () => {
     { id: 3, order: 2, text: "CARD 2" },
     { id: 4, order: 4, text: "CARD 4" },
   ]);
+
+  const [currentCard, setCurrentCard] = useState(null);
+
+  function dragStartHandler(e, card) {
+    console.log("drag", card);
+    setCurrentCard(card);
+  }
+  function dragEndHandler(e) {
+    e.target.style.background = "white";
+  }
+  function dragOverHandler(e) {
+    e.preventDefault();
+    e.target.style.background = "lightgray";
+  }
+  function dropHandler(e, card) {
+    e.preventDefault();
+    console.log("drop", card);
+    setCardList(
+      cardList.map((c) => {
+        if (c.id === card.id) {
+          return { ...c, order: currentCard.order };
+        }
+        if (c.id === currentCard.id) {
+          return { ...c, order: card.order };
+        }
+        return c;
+      })
+    );
+    e.target.style.background = "white";
+  }
+
+  const sortCards = (a, b) => {
+    if (a.order > b.order) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
+
   return (
     <div className="app">
-      {cardList.map((card) => (
-        <div className={"card"}>{card.text}</div>
+      {cardList.sort(sortCards).map((card) => (
+        <div
+          onDragStart={(e) => dragStartHandler(e, card)}
+          onDragLeave={(e) => dragEndHandler(e)}
+          onDragEnd={(e) => dragEndHandler(e)}
+          onDragOver={(e) => dragOverHandler(e)}
+          onDrop={(e) => dropHandler(e, card)}
+          draggable={true}
+          className={"card"}
+        >
+          {card.text}
+        </div>
       ))}
     </div>
   );
